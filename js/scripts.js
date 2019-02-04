77ea801adf422688b1722cbac466796e8999c6d1
Jack Collins and Robbie Kruszynski <example@example.com>
2019-02-04 11:07:12 -0800

    build foundation for page

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(event){
+  event.preventDefault();
+  $("#searchButton").click(function(){
+  window.location.href='Users/Guest/Desktop/menu/pdx.html';
+  return false;
+});
+})
