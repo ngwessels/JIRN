44127db9c9ec7fb42c972cea8d5501e36dda1813
Jack Collins and Robbie Kruszynski <example@example.com>
2019-02-04 11:37:22 -0800

    add more body to nav bars along with .html for each page

--- js/scripts.js
+++ js/scripts.js
@@ -31,8 +31,5 @@
 
 $(document).ready(function(event){
   event.preventDefault();
-  $("#searchButton").click(function(){
-  window.location.href='Users/Guest/Desktop/menu/pdx.html';
-  return false;
 });
 })
