4285ff059d6f714a27a0baa43443d783cdd115b8
Student <example@example.com>
2019-02-06 15:06:27 -0800

    build profile section of page

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,25 @@ function expand() {
   }
 }
 $('button').on('click', expand);
+
+
+
+$(document).ready(function(){
+  $("#newEntry").click(function(event){
+    event.preventDefault();
+    $("#entryForm").show();
+  });
+  $("#entryForm").submit(function(event){
+    event.preventDefault();
+    var space = $("input#place").val();
+    var thoughts = $("input#thoughts").val();
+    $("#newEntries").append("<div class=card card-default id=diaryCard>" + "<h3 class='card-title'>" + space + "</h3>" + "<br>" + "<p>" + thoughts + "</p>" + "</div>");
+    space = "";
+    thoughts = "";
+    $("input#place").value = "";
+    $("input#thoughts").value = "";
+    $("#entryForm").hide();
+  });
+});
+
+// "<div class=card>" "<div class=card-body>" "<p class=card-text>" + space + "</p>" "</div>" "</div>")
